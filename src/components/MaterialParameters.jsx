640ca18087051f6127008898c04905af279d2971
jeroldleung <jeroldleung@gmail.com>
2023-10-19 15:38:07 +0800

    display value at label of parameter items

--- src/components/MaterialParameters.jsx
+++ src/components/MaterialParameters.jsx
@@ -1,22 +1,28 @@
+import { useState } from "react";
+
 const parameterItems = [
   {
     name: "Young's modulus",
     id: "E",
     minVal: 0.001,
-    maxVal: 0.01,
+    maxVal: 0.009,
     stepVal: 0.001,
     defaultVal: 0.005,
   },
 ];
 
 function Slider({ item }) {
+  const [currentVal, setCurrentVal] = useState(item.defaultVal);
   return (
     <div className="relative">
-      <label className="py-1 text-stone-400 text-sm">{item.name}</label>
+      <label className="py-1 text-stone-400 text-sm">
+        {item.name}: {currentVal}
+      </label>
       <input
         id={item.id}
         type="range"
         defaultValue={item.defaultVal}
+        onChange={(e) => setCurrentVal(e.target.value)}
         min={item.minVal}
         max={item.maxVal}
         step={item.stepVal}
