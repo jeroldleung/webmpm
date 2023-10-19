73f9609ea09907d73b03be5a901aa4219b04aa29
jeroldleung <jeroldleung@gmail.com>
2023-10-19 15:08:01 +0800

    wrap parameter items into an array

--- src/components/MaterialParameters.jsx
+++ src/components/MaterialParameters.jsx
@@ -1,12 +1,25 @@
-function Slider(props) {
+const parameterItems = [
+  {
+    name: "Young's modulus",
+    id: "E",
+    minVal: 0.001,
+    maxVal: 0.01,
+    stepVal: 0.001,
+    defaultVal: 0.005,
+  },
+];
+
+function Slider({ item }) {
   return (
     <div className="relative">
-      <label className="py-1 text-stone-400 text-sm">{props.name}</label>
+      <label className="py-1 text-stone-400 text-sm">{item.name}</label>
       <input
+        id={item.id}
         type="range"
-        min="0"
-        max="5"
-        step="0.5"
+        defaultValue={item.defaultVal}
+        min={item.minVal}
+        max={item.maxVal}
+        step={item.stepVal}
         className="w-full h-px bg-stone-400 accent-black appearance-none cursor-pointer"
       />
     </div>
@@ -14,12 +27,11 @@ function Slider(props) {
 }
 
 export default function MaterialParameters() {
+  const items = parameterItems.map((item) => <Slider item={item} />);
   return (
     <div className="flex flex-col gap-1 sm:w-56">
       <p className="font-bold text-lg">PARAMETERS</p>
-      <Slider name="Density" />
-      <Slider name="Young's modulus" />
-      <Slider name="Bulk modulus" />
+      {items}
     </div>
   );
 }
