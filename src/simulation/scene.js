f282f78e21e44f2682c334479fa909f4b917e39d
jeroldleung <jeroldleung@gmail.com>
2024-01-11 22:16:00 +0800

    change material initial position of coupling scenes

--- src/simulation/scene.js
+++ src/simulation/scene.js
@@ -15,12 +15,12 @@ export default class Scene {
         this.objects.push(new Material(1, 2, 0.2, 0.5, [0.3, 0.4], [0.97, 0.44, 0.44, 1.0]));
         break;
       case "Fluid-Solid Coupling":
-        this.objects.push(new Material(0, 1, 0.4, 1, [0.1, 0.1], [0.06, 0.46, 0.43, 1.0]));
-        this.objects.push(new Material(1, 1, 0.2, 0.5, [0.6, 0.3], [0.97, 0.44, 0.44, 1.0]));
+        this.objects.push(new Material(0, 1, 0.4, 1, [0.2, 0.1], [0.06, 0.46, 0.43, 1.0]));
+        this.objects.push(new Material(1, 1, 0.2, 0.5, [0.5, 0.6], [0.97, 0.44, 0.44, 1.0]));
         break;
       case "Non-Sticky Fluid-Solid Coupling":
-        this.objects.push(new Material(0, 1, 0.4, 1, [0.1, 0.1], [0.06, 0.46, 0.43, 1.0]));
-        this.objects.push(new Material(1, 1, 0.2, 0.5, [0.6, 0.3], [0.97, 0.44, 0.44, 1.0]));
+        this.objects.push(new Material(0, 1, 0.4, 1, [0.2, 0.1], [0.06, 0.46, 0.43, 1.0]));
+        this.objects.push(new Material(1, 1, 0.2, 0.5, [0.5, 0.6], [0.97, 0.44, 0.44, 1.0]));
         break;
     }
   }
